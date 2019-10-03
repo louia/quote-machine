9f2d6e9ef2e7200d64b2d5b6bdec18c6c4a453ab
chov0001 <louis.chovaneck@etudiant.univ-reims.fr>
2019-10-03 12:13:10 +0200

    Crud fonctionnel + style ok

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -3,6 +3,9 @@
 
 namespace App\Controller;
 
+use App\Form\QuoteType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,44 +23,33 @@ class QuoteController extends AbstractController
     }
 
     /**
+     * @Route("/task_success", name="task_success")
+     */
+    public function task_success()
+    {
+        return $this->render('task_success.html.twig');
+    }
+
+    /**
      * @Route("/quotes", name="quotes")
      */
     public function index(Request $request)
     {
-        $quotes = [
-            [
-                "content" => "Sire, Sire ! On en a gros !",
-                "meta" => "Perceval, Livre II, Les Exploités"
-            ],
-            [
-                "content" => "Excusez ! Y a moyen de vous entretenir deux secondes ? [...] C'est au sujet que ce matin je suis passé devant vos enclos, et j'ai vu que vous aviez une jolie petite poule blanche ! [...] Une jolie petite poule vous voyez, le bel animal [...] C'est au sujet qu'en fait c'est la mienne, et que vous allez prendre un pain dans la tête, mais quelque chose de violent !",
-                "meta" => "Guethenoc, Livre III, 53 : Feue La Poule de Guethenoc"
-            ],
-            [
-                "content" => "2500 pièces d'or ???! Eh... eh... c'est un blague? 2500 pièces d'or, mais ou voulez vous que j'trouve 2500 pièces d'or, dans l'cul d'une vache ?!",
-                "meta" => "Seigneur Jacca, Livre I, 21 : La taxe militaire"
-            ],
-            [
-                "content" => "Une pluie de pierres en intérieur donc ! Je vous prenais pour un pied de chaise mais vous êtes un précurseur en fait !",
-                "meta" => "Élias de Kelliwic'h, Livre IV, Le Privilégié"
-            ],
-            [
-                "content" => "Et qu'est-ce qui font-ils, au gouvernement ? Y's'roucent les poules ! Y's'poulent les rouces ! (Guethenoc : Y's'roulent les pouces !) Voilà, mieux !",
-                "meta" => "Roparzh, Livre IV, 53 : Vox populi III"
-            ],
-        ];
+
+        $quotesStore = \SleekDB\SleekDB::store('quotes', __DIR__ . '/../../var/sleekDB');
+        $quotes = $quotesStore->fetch();
+
 
         $isResponse =false;
         $name = $request->query->get('name');
         if($name != '') {
-
             $quotess = array_filter($quotes, function ($item) use ($name) {
                 if (stripos(strtolower($item['content']), strtolower($name)) !== false) {
                     return true;
                 }
                 return false;
             });
-            if(!empty($quotess)) $isResponse =true;
+            if(!empty($quotess)) $isResponse=true;
 
             $quotes=[];
             if($isResponse) $quotes=$quotess;
@@ -67,6 +59,74 @@ class QuoteController extends AbstractController
         return $this->render('quotes.html.twig', [
             'quotes' => $quotes,'result'=>$isResponse,'query'=>$name
         ]);
+    }
+
+    /**
+     * @Route("/quotes/new", name="quotes_new")
+     */
+    public function new(Request $request)
+    {
+
+        $form = $this->createForm(QuoteType::class);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quote = $form->getData();
+            $quotesStore = \SleekDB\SleekDB::store('quotes', __DIR__ . '/../../var/sleekDB');
+
+            $quotesStore->insert($quote);
+
+            return $this->redirectToRoute('quotes');
+        }
+
+
+        return $this->render('quotes_new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/quotes/delete/{id}", name="quotes_delete")
+     */
+    public function delete($id)
+    {
+        $quotesStore = \SleekDB\SleekDB::store('quotes', __DIR__ . '/../../var/sleekDB');
+        $quotesStore->where( '_id', '=', $id )->delete();
+
+
+        return $this->redirectToRoute('quotes');
+
+    }
+
+    /**
+     * @Route("/quotes/edit/{id}", name="quotes_edit")
+     */
+    public function edit($id,Request $request)
+    {
+        $quotesStore = \SleekDB\SleekDB::store('quotes', __DIR__ . '/../../var/sleekDB');
+        $quote = $quotesStore->where( '_id', '=', $id )->fetch();
+
+
+        $form = $this->createForm(QuoteType::class,$quote[0]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quote = $form->getData();
+            $quotesStore->where( '_id', '=', $id )->update($quote);
+
+            return $this->redirectToRoute('quotes');
+        }
+
+
+        return $this->render('quotes_edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+
+
     }
 
 }
---
+++ src/Form/QuoteType.php
@@ -0,0 +1,36 @@
+<?php
+// src/Form/TaskType.php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Yaml\Tests\A;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextType::class,[
+                'label'=>'La citation',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 3]),
+                ],
+            ])
+            ->add('meta', TextType::class,[
+                'label'=>'Les métadonées',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 3]),
+                ],
+            ],)
+            ->add('save', SubmitType::class, ['label' => 'Envoyer'])
+        ;
+    }
+}
