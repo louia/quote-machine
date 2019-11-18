ebd8650e03221bf71fee14ec171da9d76479ea62
louislinux <louis.chovaneck@gmail.com>
2019-11-18 19:45:39 +0100

    functionnal test of HelloWorld

--- tests/Controller/HelloWorldTest.php
+++ tests/Controller/HelloWorldTest.php
@@ -17,5 +17,18 @@ class HelloWorldTest extends WebTestCase
             $client->getResponse()->getContent()
         );
         $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('html body', 'Hello World!');
+    }
+
+    public function testHelloWorldWithParams()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/louis');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('html body', 'Hello Louis !');
     }
 }
