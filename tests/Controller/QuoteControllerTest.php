919723fbcd066f0fe6bb9e7d30d3f31ad452968b
chov0001 <louis.chovaneck@etudiant.univ-reims.fr>
2019-12-12 12:04:05 +0100

    test CRUD quote (manque edit)

--- tests/Controller/QuoteControllerTest.php
+++ tests/Controller/QuoteControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Controller;
 
 use App\Entity\Citation;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class QuoteControllerTest extends WebTestCase
 {
@@ -23,7 +24,33 @@ class QuoteControllerTest extends WebTestCase
 
     public function testNew()
     {
-        $client = static::createClient();
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'louis',
+            'PHP_AUTH_PW' => '1234',
+        ]);
+        $client->followRedirects();
+        $crawler = $client->request('GET', '/quotes/new');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $form = $crawler->selectButton('Envoyer')->form();
+        $form['quote[content]']->setValue('Symfony Rocks!');
+        $form['quote[meta]']->setValue('Louis Chovaneck');
+        $client->submit($form);
+
+        $cit = $this->entityManager
+            ->getRepository(Citation::class)
+            ->findOneBy(['meta' => 'Louis Chovaneck']);
+
+        $this->assertSame('Symfony Rocks!', $cit->getContent());
+    }
+
+    public function testNewDelete()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'louis',
+            'PHP_AUTH_PW' => '1234',
+        ]);
         $client->followRedirects();
         $crawler = $client->request('GET', '/quotes/new');
 
@@ -40,6 +67,65 @@ class QuoteControllerTest extends WebTestCase
             ->findOneBy(['meta' => 'Louis Chovaneck']);
 
         $this->assertSame('Symfony Rocks!', $cit->getContent());
+
+        $crawler = $client->request('GET', '/quotes/delete/'.$cit->getId());
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $cit = $this->entityManager
+            ->getRepository(Citation::class)
+            ->findOneBy(['meta' => 'Louis Chovaneck']);
+
+        $this->assertSame(null, $cit);
+    }
+
+    public function testNewShow()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'louis',
+            'PHP_AUTH_PW' => '1234',
+        ]);
+        $client->followRedirects();
+        $crawler = $client->request('GET', '/quotes/new');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Envoyer')->form();
+        $form['quote[content]']->setValue('Symfony Rocks!');
+        $form['quote[meta]']->setValue('Louis Chovaneck');
+        $client->submit($form);
+
+        $cit = $this->entityManager
+            ->getRepository(Citation::class)
+            ->findOneBy(['meta' => 'Louis Chovaneck']);
+
+        $this->assertSame('Symfony Rocks!', $cit->getContent());
+
+        $crawler = $client->request('GET', '/quotes');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $found = false;
+        $numberOfPages = $crawler->filter('ul.pagination-list li')->count();
+        for ($i = 1; $i <= $numberOfPages; ++$i) {
+            $myQuote = $crawler->filter('p.title')->each(function (Crawler $node, $i) {
+                return $node->text();
+            });
+
+            for ($ii = 1; $ii <= count($myQuote); ++$ii) {
+                if ($myQuote[$ii - 1] == $cit->getContent()) {
+                    $find = $myQuote[$ii - 1];
+                    $found = true;
+                }
+            }
+            if (!$found) {//Si pas trouver dans la page : page suivante
+                $crawler = $client->request('GET', '/quotes?page='.($i + 1));
+            }
+        }
+        $this->assertEquals(true, $found);
     }
 
     protected function tearDown()
