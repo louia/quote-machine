d7858f149f8c0e4dfb3b13b950fb509bf5148abe
louislinux <louis.chovaneck@gmail.com>
2020-01-10 19:08:09 +0100

    test command :white_check_mark:

--- tests/Command/RandomQuoteTest.php
+++ tests/Command/RandomQuoteTest.php
@@ -10,23 +10,16 @@ class CreateUserCommandTest extends KernelTestCase
 {
     public function testExecute()
     {
-//        $kernel = static::createKernel();
-//        $application = new Application($kernel);
-//
-//        $command = $application->find('app:random-quote --category Voyage');
-//        $commandTester = new CommandTester($command);
-//        $commandTester->execute([
-//        // pass arguments to the helper
-//        'username' => 'Wouter',
-//
-//        // prefix the key with two dashes when passing options,
-//        // e.g: '--some-option' => 'option_value',
-//        ]);
-//
-//        // the output of the command in the console
-//        $output = $commandTester->getDisplay();
-//        $this->assertContains('Username: Wouter', $output);
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
 
-        // ...
+        $command = $application->find('app:random-quote');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            '--category' => 'Voyage',
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertContains('je suis beau', $output);
     }
 }
