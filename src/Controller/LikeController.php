df61d559c6dfad24024b7f7d561d4b07ceccf667
louislinux <louis.chovaneck@gmail.com>
2020-01-28 19:35:51 +0100

    like presque fini !! ajout ajax avec jquery + style

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -5,48 +5,60 @@ namespace App\Controller;
 use App\Entity\Citation;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LikeController extends AbstractController
 {
     /**
-     * @Route("/add/like/{id}", name="like")
+     * @Route("/like/add/{id}", name="like")
+     *
+     * @return JsonResponse
      */
     public function like(Citation $quote)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
         /** @var User $user */
         $user = $this->getUser();
-        $quote->addUser($user);
+        $response = new JsonResponse();
+        if ($user !== $quote->getAuthor()) {
+            $response->setData(['code' => 'ok']);
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($quote);
-        $entityManager->flush();
+            $quote->addUser($user);
 
-        return new Response('ok');
-//        return $this->render('like/index.html.twig', [
-//            'controller_name' => 'LikeController',
-//        ]);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($quote);
+            $entityManager->flush();
+        } else {
+            $response->setData(['code' => 'notok']);
+        }
+
+        return $response;
     }
 
     /**
-     * @Route("/add/dislike/{id}", name="dislike")
+     * @Route("/like/remove/{id}", name="dislike")
+     *
+     * @return JsonResponse
      */
     public function dislike(Citation $quote)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+        $response = new JsonResponse();
+
         /** @var User $user */
         $user = $this->getUser();
-        $quote->removeUser($user);
+        if ($user !== $quote->getAuthor()) {
+            $response->setData(['code' => 'ok']);
+            $quote->removeUser($user);
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($quote);
-        $entityManager->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($quote);
+            $entityManager->flush();
+        } else {
+            $response->setData(['code' => 'notok']);
+        }
 
-        return new Response('ok');
-//        return $this->render('like/index.html.twig', [
-//            'controller_name' => 'LikeController',
-//        ]);
+        return $response;
     }
 }
